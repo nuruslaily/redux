cfbfa55aebf75320614121ab4b383d9cdadd859d
nuruslaily <nuruslaily88@gmail.com>
2021-03-17 11:30:27 +0700

    praktikum redux done

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import CreateToDo from './Containers/CreateTodo'
+import Table from './Containers/Table'
+
+function App() {
+  return (
+    <div>
+      <CreateToDo />
+      <Table />
+    </div>
+  );
+}
+
+export default App;
